7c3869b88b2baee9bb89be64ac8193526b49b097
Vicious <137929047+Darkmixt12@users.noreply.github.com>
2025-11-15 16:42:20 -0600

    fix: kanban-table spec fixed

--- apps/steven-munoz/src/test-setup.ts
+++ apps/steven-munoz/src/test-setup.ts
@@ -1,5 +1,10 @@
 import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
 
+(globalThis as any).fetch = () =>
+  Promise.resolve({
+    json: () => Promise.resolve({}),
+  });
+
 setupZoneTestEnv({
   errorOnUnknownElements: true,
   errorOnUnknownProperties: true,
