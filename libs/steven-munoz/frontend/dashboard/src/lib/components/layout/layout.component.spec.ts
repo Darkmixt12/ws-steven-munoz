69f011799590d3b6eb1fa3530bfc808e4940763a
Vicious <137929047+Darkmixt12@users.noreply.github.com>
2025-12-16 10:25:42 -0600

    mock

--- libs/steven-munoz/frontend/dashboard/src/lib/components/layout/layout.component.spec.ts
+++ libs/steven-munoz/frontend/dashboard/src/lib/components/layout/layout.component.spec.ts
@@ -1,12 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LayoutComponent } from './layout.component';
 import { provideRouter, RouterModule } from '@angular/router';
+import { Firestore } from '@angular/fire/firestore';
+import { of } from 'rxjs';
 
 jest.mock('@angular/fire/firestore', () => ({
-  collection: jest.fn(),
-  collectionData: jest.fn(),
   Firestore: jest.fn(),
+  collection: jest.fn(),
+  collectionData: jest.fn(() => of([])),
 }));
+
 describe('Layout', () => {
   let component: LayoutComponent;
   let fixture: ComponentFixture<LayoutComponent>;
@@ -14,7 +17,10 @@ describe('Layout', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [LayoutComponent, RouterModule],
-      providers: [provideRouter([])],
+      providers: [
+        provideRouter([]),
+        { provide: Firestore, useValue: {} },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LayoutComponent);
