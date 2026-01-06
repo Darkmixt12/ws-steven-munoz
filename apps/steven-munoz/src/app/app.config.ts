e82995c404bddbf51c5ea31801e561942e951808
Vicious <137929047+Darkmixt12@users.noreply.github.com>
2026-01-06 00:31:40 -0600

    PrimeNG Table ,  Dialog and Form to CreateCLient complete

--- apps/steven-munoz/src/app/app.config.ts
+++ apps/steven-munoz/src/app/app.config.ts
@@ -6,14 +6,22 @@ import {
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import Aura from '@primeng/themes/aura'
 import { providePrimeNG } from 'primeng/config';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from './environment';
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+      providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false
+        },
+      }
+    }),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
--- libs/steven-munoz/frontend/dashboard/src/lib/services/firestore.service.ts
+++ libs/steven-munoz/frontend/dashboard/src/lib/services/firestore.service.ts
@@ -1,10 +1,22 @@
-import { Injectable } from '@angular/core';
-import {deleteDoc, DocumentData, DocumentReference, updateDoc} from '@angular/fire/firestore';
+import { inject, Injectable } from '@angular/core';
+import {
+  addDoc,
+  collection,
+  collectionData,
+  deleteDoc,
+  DocumentData,
+  DocumentReference,
+  Firestore,
+  updateDoc,
+} from '@angular/fire/firestore';
+import { Client, CreateClient } from '../types/client.interface';
+import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root',
 })
 export class FirestoreService {
-  
+  firestore = inject(Firestore);
 
   updateDoc(ref: DocumentReference<DocumentData, DocumentData>, columns: any){
     console.log('entramos al service')
@@ -13,5 +25,15 @@ export class FirestoreService {
 
   deleteDoc(ref: DocumentReference<any>) {
     return deleteDoc(ref)
+  }
+
+  getClients(): Observable<Client[]> {
+    const ref = collection(this.firestore, 'clients');
+    return collectionData(ref) as Observable<Client[]>;
+  }
+
+  createClient(client: CreateClient) {
+    const clientsRef = collection(this.firestore, 'clients');
+    return addDoc(clientsRef, client);
   }
 }
